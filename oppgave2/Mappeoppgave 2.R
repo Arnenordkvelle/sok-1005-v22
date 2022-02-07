b2e7598152b73c9e5b1996e8b6f410961b9e130d
Your Name <you@example.com>
2022-02-07 12:58:22 +0100

    mappeoppgave 2

---
+++ oppgave2/Mappeoppgave 2.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(jsonlite)
+library(scales)
+library(ggrepel)
+
+UScovid <- fromJSON("https://static01.nyt.com/newsgraphics/2021/12/20/us-coronavirus-deaths-2021/ff0adde21623e111d8ce103fedecf7ffc7906264/scatter.json")
+
+UScovid$name <- state.abb[match(UScovid$name, state.name)]
+
+UScovid[is.na(UScovid)] <- "DC"
+
+UScovid %>%
+  ggplot(aes(x=fully_vaccinated_pct_of_pop, y=deaths_per_100k, label = name)) +
+  geom_point(size = 4, shape = 20, col="palegreen4", fill = "palegreen4", stroke = 0.1, alpha = 1) + geom_text_repel() +
+  scale_x_continuous(labels = scales::percent, limits=c(0.45, 0.80), breaks=seq(0.45, 0.80, by = 0.05)) +
+  labs(title="20 avg. monthly deaths per 100,000",
+       x ="Share of total population fully vaccinated",
+       y = "Monthly deaths per 100,000") +
+  theme_bw()
+
+
+lm(deaths_per_100k ~ fully_vaccinated_pct_of_pop, data = UScovid)
+
+UScovid %>%
+  ggplot(aes(x=fully_vaccinated_pct_of_pop, y=deaths_per_100k, label = name)) +
+  geom_point(size = 4, shape = 21, col="palegreen4", fill = "palegreen4", stroke = 0.3, alpha = 0.7) + geom_text_repel(box.padding = unit(0.5, "lines")) +
+  geom_smooth(method = lm) +
+  scale_x_continuous(labels = scales::percent, limits=c(0.45, 0.80), breaks=seq(0.45, 0.80, by = 0.05)) +
+  labs(title="20 avg. monthly deaths per 100,000",
+       x ="Share of total population fully vaccinated",
+       y = "Monthly deaths per 100,000") +
+  theme_bw()
+# Den blå lineære linja på grafen tror jeg skal representere gjennomsnittet og den svarte området viser koeffisienten 
+# Linja viser en nedgang av dødsfall desto mer prosent av befolkningen er vaksinert. Det er en tydelig korrelasjon mellom antall prosent vaksinerte og dødsfall.
