c24dae6770cc0a9fbefa1037246cf889359143c0
Your Name <you@example.com>
2022-02-07 17:37:18 +0100

    Mappeoppgave 2

--- oppgave2/Mappeoppgave 2.R
+++ oppgave2/Mappeoppgave 2.R
@@ -30,7 +30,7 @@ lm(deaths_per_100k ~ fully_vaccinated_pct_of_pop, data = UScovid)
 # Plot 2
 UScovid %>%
   ggplot(aes(x=fully_vaccinated_pct_of_pop, y=deaths_per_100k, label = name)) +
-  geom_point(size = 4, shape = 21, col="palegreen4", fill = "palegreen4", stroke = 0.3, alpha = 0.7) + geom_text_repel(box.padding = unit(0.5, "lines")) +
+  geom_point(size = 3, shape = 21, col="palegreen4", fill = "palegreen4", stroke = 1, alpha = 0.5) + geom_text_repel(box.padding = unit(0.5, "lines")) +
   geom_smooth(method = lm) +
   scale_x_continuous(labels = scales::percent, limits=c(0.45, 0.80), breaks=seq(0.45, 0.80, by = 0.05)) +
   labs(title="20 avg. monthly deaths per 100,000",
