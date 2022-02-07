3608911ca19e39cbdb95bf2f91a50cbef034190b
Your Name <you@example.com>
2022-02-07 17:19:18 +0100

    Mappeoppgave 2

--- oppgave2/Mappeoppgave 2.R
+++ oppgave2/Mappeoppgave 2.R
@@ -13,6 +13,7 @@ UScovid$name <- state.abb[match(UScovid$name, state.name)]
 # alle "NA" verdier om til "DC" for å få objektet med på plottet.
 UScovid[is.na(UScovid)] <- "DC"
 
+# Plot 1
 UScovid %>%
   ggplot(aes(x=fully_vaccinated_pct_of_pop, y=deaths_per_100k, label = name)) +
   geom_point(size = 4, shape = 20, col="palegreen4", fill = "palegreen4", stroke = 0.1, alpha = 1) + geom_text_repel(box.padding = unit(0.1, "lines")) +
@@ -25,6 +26,7 @@ UScovid %>%
 
 lm(deaths_per_100k ~ fully_vaccinated_pct_of_pop, data = UScovid)
 
+# Plot 2
 UScovid %>%
   ggplot(aes(x=fully_vaccinated_pct_of_pop, y=deaths_per_100k, label = name)) +
   geom_point(size = 4, shape = 21, col="palegreen4", fill = "palegreen4", stroke = 0.3, alpha = 0.7) + geom_text_repel(box.padding = unit(0.5, "lines")) +
@@ -34,6 +36,7 @@ UScovid %>%
        x ="Share of total population fully vaccinated",
        y = "Monthly deaths per 100,000") +
   theme_bw()
+
 # Den blå lineære linja på grafen tror jeg skal representere gjennomsnittet og den svarte området viser koeffisienten.
 # Linja viser en nedgang av dødsfall desto mer prosent av befolkningen er vaksinert. 
 # Det er en tydelig korrelasjon mellom antall prosent vaksinerte og dødsfall.
