a64889b6d2eebfc4d64d4b761b295d4d69fcb9f6
Arne Nordkvelle <ano141@uit.no>
2022-02-17 21:42:42 +0100

    Mappeoppgave 3

--- oppgave3/Mappeoppgave 3.R
+++ oppgave3/Mappeoppgave 3.R
@@ -1,68 +1,8 @@
-<<<<<<< HEAD
-=======
 # Nødvendige pakker
 
->>>>>>> 1ea309dd288822ac77e53a96a1526dd5c58c1413
 library(tidyverse)
 library(rvest)
 library(proto)
-
-<<<<<<< HEAD
-data <- read_html("https://www.motor.no/aktuelt/motors-store-vintertest-av-rekkevidde-pa-elbiler/217132")
-
-data <- data %>% html_table()
-
-data <- data[[1]]
-
-data <- data %>% 
-  rename(Modell = X1,
-         WLTP = X2,
-         STOPP = X3,
-         Avvik = X4)
-
-data = select(data, -c(1, 4))
-
-data =data[-1,]
-
-data <-data[!grepl("x",data$STOPP),]
-
-data$STOPP<-gsub("km","",as.character(data$STOPP))
-
-data$STOPP <- as.numeric(as.character(data$STOPP))
-
-data$WLTP <- sub("^(\\d{3}).*$", "\\1",data$WLTP)
-
-# kilde: https://stackoverflow.com/questions/21675379/r-only-keep-the-3-x-first-characters-in-a-all-rows-in-a-column/21675473
-
-data$WLTP <- as.numeric(as.character(data$WLTP))
-
-data %>%
-  ggplot(aes(x=WLTP, y=STOPP)) +
-  geom_point(size = 2.5, col="black") +
-  geom_abline(size = 1.75, col = "red") +
-  scale_x_continuous(limits= c(200, 600), breaks = seq(200, 600, by = 100)) +
-  scale_y_continuous(limits= c(200, 600), breaks = seq(200, 600, by = 100)) +
-  labs(title="Plot",
-       x ="WLTP",
-       y = "STOPP") +
-  theme_bw()
-
-lm(STOPP ~ WLTP, data =data)
-
-data %>%
-  ggplot(aes(x=WLTP, y=STOPP)) +
-  geom_point(size = 2.5, col="black") +
-  geom_abline(size = 1.75, col = "red") +
-  geom_smooth(method = lm) +
-  scale_x_continuous(limits= c(200, 600), breaks = seq(200, 600, by = 100)) +
-  scale_y_continuous(limits= c(200, 600), breaks = seq(200, 600, by = 100)) +
-  labs(title="Plot",
-       x ="WLTP",
-       y = "STOPP") +
-  theme_bw()
-
-=======
-# Laster inn linken
 
 data <- read_html("https://www.motor.no/aktuelt/motors-store-vintertest-av-rekkevidde-pa-elbiler/217132")
 
@@ -140,9 +80,6 @@ data %>%
   theme_bw()
 
 # Den blå regresjonslinja viser at bilene er i gjennomsnitt ganske nærme på å ha rekkeviden de reklamerer å ha.
-# 
-
-
->>>>>>> 1ea309dd288822ac77e53a96a1526dd5c58c1413
+#
 
 
